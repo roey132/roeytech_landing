f46bb7fd88bd454e1f569f46cb684444e022f04a
roey132 <roey132@gmail.com>
2025-08-14 14:27:56 +0700

    turn site back to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
 
 export default defineConfig({
-  output: "server",
-  adapter: node({ mode: "standalone" }),
+  output: "static", // static build output
   vite: { plugins: [tailwindcss()] },
 });
